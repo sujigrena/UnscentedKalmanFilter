e7ad1f7a4cf8e35720623107ce81a6062b84c4de
sujigrena <sujigrena@gmail.com>
2018-03-31 02:58:58 +0530

    initial commit

--- src/ukf.h
+++ src/ukf.h
@@ -66,6 +66,12 @@ public:
 
   ///* Sigma point spreading parameter
   double lambda_;
+ 
+///* the current NIS for radar
+  double NIS_radar_;
+
+  ///* the current NIS for laser
+  double NIS_laser_;
 
 
   /**
